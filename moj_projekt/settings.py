8892fd9dc98e791320e34fa59172bd43bd076231
ivanhodzic <ivanhodzic@gmail.com>
2025-03-14 00:33:17 +0100

    Updates settings configuration
    Changes BASE_DIR computation to use os.path functions
    Adds '127.0.0.1' to ALLOWED_HOSTS for local development

--- moj_projekt/settings.py
+++ moj_projekt/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'moj-projekt.onrender.com',
+    '127.0.0.1',
 ]
 
 
